5600513624275c379e6b91f078bdb3a680b747e3
Pranay Jain <pranay1117@yahoo.co.in>
2018-03-27 19:32:33 -0700

    updates

--- projects/stationf/app/components/Results.jsx
+++ projects/stationf/app/components/Results.jsx
@@ -2,38 +2,65 @@ import React from 'react';
 import '../styles/results.scss';
 
 export default class Results extends React.Component {
+	componentWillMount() {
+		this.renderRooms = this.renderRooms.bind(this);
+		this.renderRoom = this.renderRoom.bind(this);
+	}
+
 	render() {
 		let i = 0;
 		return (
 			<div className="results">
 				<div className="results-title">Available Rooms</div>
-				<div className="results-rooms" onClick={() => this.props.setCurrentRoom("room1")}>
-					<div className="results-rooms-room">
-						<img className="img" src={require(`../assets/room${i === 0 ? '1' : '2'}.jpg`)} />
-						<div className="results-rooms-room-content">
-							<div className="results-rooms-room-content-title">Room 1</div>
-						</div>
-					</div>
-					<div className="results-rooms-room">
-						<img className="img" src={require(`../assets/room2.jpg`)} />
-						<div className="results-rooms-room-content">
-							<div className="results-rooms-room-content-title">Room 2</div>
-							<div className="results-rooms-room-content-details">
-								<div className="text">Capacity</div>
-								<div className="text">12</div>
-								<div className="text equip">Equipment</div>
-								<div className="results-rooms-room-content-details-equip">
-									<div className="results-rooms-room-content-details-equip-item"><img src={require('../assets/projector.png')} /></div>
-									<div className="results-rooms-room-content-details-equip-item"><img src={require('../assets/tv.png')} /></div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="results-rooms-room">
+				<div className="results-rooms" >
+					{this.renderRooms()}
+				</div>
+			</div>
+		);
+	}
 
+	renderRooms() {
+		let renderedRooms = [];
+		let i = 1;
+		for (let room of this.props.rooms) {
+			renderedRooms.push(this.renderRoom(room, i));
+			i++;
+		}
+
+		return renderedRooms;
+	}
+
+	renderRoom(room, index) {
+		return(
+			<div className="results-rooms-room" onClick={() => this.props.setCurrentRoom(room)}>
+				<img className="img" src={require(`../assets/room${index}.jpg`)} />
+				<div className="results-rooms-room-content">
+					<div className="results-rooms-room-content-title">{room.name}</div>
+					<div className="results-rooms-room-content-details">
+						<div className="text">Capacity</div>
+						<div className="text">{room.capacity}</div>
+						<div className="text equip">Equipment</div>
+						<div className="results-rooms-room-content-details-equip">
+							{
+								this.renderEquipmentIcons(room.equipements).length > 0 ?
+								this.renderEquipmentIcons(room.equipements)
+								:
+								'N/A'
+							}
+						</div>
 					</div>
 				</div>
 			</div>
 		);
 	}
+
+	renderEquipmentIcons(equipments) {
+		let renderedEquips = [];
+		for (let equip of equipments) {
+			renderedEquips.push(<div className="results-rooms-room-content-details-equip-item"><img src={require(`../assets/${equip.name === "TV" ? "tv" : "projector"}.png`)} /></div>);
+		}
+
+		return renderedEquips;
+	}
+
 }
--- projects/stationf/app/index.jsx
+++ projects/stationf/app/index.jsx
@@ -17,39 +17,111 @@ class App extends React.Component {
 			numberSeats: 0,
 			equip: [],
 			isFiltered: false,
-			currentRoom: null
+			currentRoom: null,
+			rooms: []
 		};
 	}
 
 	componentWillMount() {
 		this.setCurrentRoom = this.setCurrentRoom.bind(this);
 
+		this.setState({
+			...this.state,
+			rooms: [
+				      {
+				         "name":"Salle #1",
+				         "description":"Salle #1",
+				         "capacity":5,
+				         "equipements":[
+				            {
+				               "name":"TV"
+				            },
+				            {
+				               "name":"Retro Projecteur"
+				            }
+				         ],
+				         "createdAt":"2016-12-07T12:39:29.812Z",
+				         "updatedAt":"2016-12-08T17:31:39.489Z"
+				      },
+				      {
+				         "name":"Salle #2",
+				         "description":"Salle #2",
+				         "capacity":10,
+				         "equipements":[
+				            {
+				               "name":"Retro Projecteur"
+				            }
+				         ],
+				         "createdAt":"2016-12-07T12:39:55.384Z",
+				         "updatedAt":"2016-12-07T13:33:37.184Z"
+				      },
+				      {
+				         "name":"Salle Okjsdkso",
+				         "description":"Salle Okjsdkso",
+				         "capacity":11,
+				         "equipements":[],
+				         "createdAt":"2016-12-07T14:15:55.733Z",
+				         "updatedAt":"2016-12-09T16:45:19.025Z"
+				      },
+				      {
+				         "name":"Salle de ouf",
+				         "description":"Salle de ouf",
+				         "capacity":10,
+				         "equipements":[
+				            {
+				               "name":"TV"
+				            },
+				            {
+				               "name":"Retro Projecteur"
+				            }
+				         ],
+				         "createdAt":"2016-12-09T16:45:34.419Z",
+				         "updatedAt":"2016-12-09T16:45:34.419Z"
+				      },
+				      {
+				         "name":"Salle nulle",
+				         "description":"Salle nulle",
+				         "capacity":26,
+				         "equipements":[
+				            {
+				               "name":"TV"
+				            },
+				            {
+				               "name":"Retro Projecteur"
+				            }
+				         ],
+				         "createdAt":"2016-12-09T16:45:49.096Z",
+				         "updatedAt":"2016-12-09T16:45:49.096Z"
+				      }
+			   ]
+		});
+
 		fetch('https://online.stationf.co/tests/rooms.json', {
 			method: 'GET'
 		})
-		  .then((response) => {
-		  	console.log("Hellow rodl");
-		      if (response.status !== 200) {
-		        console.log(response);
-		        return;
-		      }
+		//   .then((response) => {
+		//   	console.log("Hellow rodl");
+		//       if (response.status !== 200) {
+		//         console.log(response);
+		//         return;
+		//       }
 
-		      // Examine the text in the response
-		      response.json().then(function(data) {
-		      	console.log(data);
-		      });
-		    }
-		  )
-		  .catch((err) => {
-		    console.log('Fetch Error :-S', err);
-		  });
+		//       // Examine the text in the response
+		//       response.json().then(function(data) {
+		//       	console.log(data);
+		//       });
+		//     }
+		//   )
+		//   .catch((err) => {
+		//     console.log('Fetch Error :-S', err);
+		//   });
 	}
 
 	render() {
 		return (
 			<div>
 				<LeftBar />
-				<Results setCurrentRoom={this.setCurrentRoom} />
+				<Results rooms={this.state.rooms} setCurrentRoom={this.setCurrentRoom} />
 				{ this.state.currentRoom ? 
 					<RoomDialog />
 					:
