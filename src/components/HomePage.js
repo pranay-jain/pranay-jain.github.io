58ad8b87bb52376ecfa85d59717ceeb4e2b5b09f
Pranay Jain <pranay1117@yahoo.co.in>
2023-01-04 23:19:34 -0500

    updated to duke

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -43,29 +43,22 @@ export default function HomePage() {
           </Styled.h1>
           <SectionTitle title="BACKGROUND" />
           <Styled.p>
-            I am a recent undergraduate from the University of British Columbia
-            where I studied Computer Science and Mathematics. My research
-            interests lie at the intersection of Algorithmic Game Theory and
-            Artificial Intelligence. I currently work at the UBC{" "}
-            <ExtLink href="https://www.cs.ubc.ca/cs-research/lci">
-              Lab for Computational Intelligence
-            </ExtLink>
-            , where I'm fortunate to be advised by{" "}
-            <Link href="https://www.cs.ubc.ca/~kevinlb/" sx={LinkStyle}>
-              Kevin Leyton-Brown
-            </Link>
-            .
+            I am a masters student Computer Science at Duke University. I am 
+            interested in the fields of machine learning, distributed systems and security.
+            I completed my undergraduate degree in Computer Science and Mathematics
+            from the University of British Columbia.
           </Styled.p>
           <Styled.p>
             I've previous experience as a full-stack software developer and have
             worked with a few startups to build their products. Some of them
-            include <ExtLink href="https://dapperlabs.com">Dapper Labs</ExtLink>,{" "}
+            include <ExtLink href="https://www.moderntreasury.com/">Modern Treasury</ExtLink>,{" "}
+            <ExtLink href="https://dapperlabs.com">Dapper Labs</ExtLink>,{" "}
             <ExtLink href="https://collabware.com/">Collabware</ExtLink>,{" "}
             <ExtLink href="http://vitay.io/">Vitay</ExtLink> and{" "}
             <ExtLink href="http://featherx.ai">FeatherX</ExtLink>.
           </Styled.p>
 
-          <Box sx={{ marginY: 5 }}>
+          {/* <Box sx={{ marginY: 5 }}>
             <SectionTitle title="RESEARCH" />
             My current research relates to the{" "}
             <ExtLink href="https://www.fcc.gov/about-fcc/fcc-initiatives/incentive-auctions">
@@ -76,7 +69,7 @@ export default function HomePage() {
             on developing a generative model for constraint graphs that can be
             used for simulating reverse auctions in different spectrum auction
             settings.
-          </Box>
+          </Box> */}
 
           <Box sx={{ marginY: 5 }}>
             <SectionTitle title="SELECTED PROJECTS" />
